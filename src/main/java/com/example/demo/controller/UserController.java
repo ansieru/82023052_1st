a155c9d59f94d853f542e60239e8cb97d270ef13
ansieru <ansieru@gmail.com>
2024-10-21 21:18:44 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    // GET 방식으로 사번을 반환하는 API
+    @GetMapping("/user")
+    public String getUserId(@PathVariable("employeeId") String employeeId) {
+        // employeeId를 경로에서 추출해 사번을 리턴
+        return "사번: " + employeeId;
+    }
+}
